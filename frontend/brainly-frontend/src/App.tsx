d51fb33bffce28b33746280ebe7bfdfb1ffd5fda
XxXSurferXx <sharmarahul5889@gmail.com>
2026-06-20 23:18:03 +0530

    Added frontend with 2 buttons

---
+++ frontend/brainly-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components/ui/Button'
+
+function App() {
+  
+  return (
+    <>
+      <Button variant="primary" size="md" text="Share" onClick={() => {}} /> 
+      <Button variant="secondary" size="md" text="Add Content" onClick={() => {}} /> 
+    </>
+  )
+}
+
+export default App
---
+++ frontend/brainly-frontend/src/components/ui/Button.tsx
@@ -0,0 +1,21 @@
+import type {ReactElement} from 'react';
+
+type Variants = "primary" | "secondary";
+
+export interface ButtonProps {
+    variant: Variants;
+    size: "sm" | "md" | "lg";
+    text: string;
+    startIcon?: ReactElement;
+    endIcon?: ReactElement;
+    onClick: () => void;
+}
+
+const variantStyles = {
+    "primary": "bg-purple-600 text-white",
+    "secondary": "bg-purple-400 text-purple-600"
+}
+
+export const Button = (props: ButtonProps) => {
+    return <button className = {variantStyles[props.variant]}>{props.text}</button>
+}
